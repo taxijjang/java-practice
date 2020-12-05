9ad922790fc555b10aa228677ecd30c78a0255e2
taxijjang <gw9122@naver.com>
2020-12-05 13:35:00 +0900

    taco-clode 2장

--- taco-cloud/src/main/java/tacos/web/DesignTacoController.java
+++ taco-cloud/src/main/java/tacos/web/DesignTacoController.java
@@ -4,26 +4,27 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import javax.validation.Valid;
+
+import org.springframework.validation.Errors;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import lombok.extern.slf4j.Slf4j;
 import tacos.Taco;
 import tacos.Ingredient;
 import tacos.Ingredient.Type;
 
-
 @Slf4j
 @Controller
 @RequestMapping("/design")
 public class DesignTacoController {
 
-    @ModelAttribute
-    public void addIngredientsToModel(Model model) {
+    @GetMapping
+    public String showDesignForm(Model model) {
         List<Ingredient> ingredients = Arrays.asList(
                 new Ingredient("FLTO", "Flour Tortilla", Type.WRAP),
                 new Ingredient("COTO", "Corn Tortilla", Type.WRAP),
@@ -42,19 +43,12 @@ public class DesignTacoController {
             model.addAttribute(type.toString().toLowerCase(),
                     filterByType(ingredients, type));
         }
-    }
 
-    @GetMapping
-    public String showDesignForm(Model model) {
-        model.addAttribute("design", new Taco());
+        model.addAttribute("taco", new Taco());
+
         return "design";
     }
 
-    @PostMapping
-    public String processDesign(Taco design){
-        log.info("Processing design: " + design);
-        return "redirect:/orders/current";
-    }
     private List<Ingredient> filterByType(
             List<Ingredient> ingredients, Type type) {
         return ingredients
@@ -62,4 +56,18 @@ public class DesignTacoController {
                 .filter(x -> x.getType().equals(type))
                 .collect(Collectors.toList());
     }
+
+    @PostMapping
+    public String processDesign(@Valid Taco design, Errors errors) {
+        if (errors.hasErrors()) {
+            return "design";
+        }
+
+        // 이 지점에서 타코 디자인(선택된 식재료 내역)을 저장한다…
+        // 이 작업은 3장에서 할 것이다
+        log.info("Processing design: " + design);
+
+        return "redirect:/orders/current";
+    }
+
 }
